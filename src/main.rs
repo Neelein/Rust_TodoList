2b7b112dd04f3407a17b58f1ff2a0010efdcd1ec
Neelein <shakya12210305@gmail.com>
2024-02-10 20:21:23 +0545

    202402102021

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use todo_list; 
+use todo_list::Task;
+
+use std::io::Write;
+use std::io::stdin;
+use std::io::stdout;
+
+fn runprompt(todo: &mut Vec<Task>){
+    loop{
+        let mut stdout = stdout();
+        print!("(todo list) > ");
+        stdout.flush().expect("can't flush the stdout");
+
+        let mut buffer = String::new();
+        let args:Vec<&str> = buffer.split_whitespace().collect();
+
+        // todo_list::run(args,todo);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut todo:Vec<Task> = Vec::new();
+    runprompt(&mut todo);
 }
---
+++ todo_list/src/lib.rs
@@ -0,0 +1,12 @@
+#[derive(Debug)]
+pub struct Task{
+    task: String,
+    done_status: bool, 
+    id: u64, 
+}
+
+// pub fn run(args: Vec<&str>, todo: &mut Vec<Task>){
+
+//     parse_arguments(args, todo); 
+        
+// }
